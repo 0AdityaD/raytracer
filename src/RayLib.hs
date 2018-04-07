1cce4e67ff2816639429653729888e6f01e27c89
Aditya Durvasula <d4aditya@gmail.com>
2018-04-07 16:54:05 -0500

    complete phong illumination model except shadows

--- src/RayLib.hs
+++ src/RayLib.hs
@@ -6,6 +6,9 @@ data Ray = Ray {rayOrigin :: Vector Double,
                 rayDirection :: Vector Double}
     deriving (Show, Eq)
 
+eps :: Double
+eps = 0.000001
+
 data Isect = Isect {iT :: Double,
                     iNormal :: Vector Double,
                     iObject :: Geometry,
@@ -23,27 +26,30 @@ instance At Double where
 instance At Isect where 
     at (Isect t _ _ _) (Ray p d) = p + (fromList [t] :: Vector Double) * d
 
-type MatParam = Isect -> Vector Double
+type Color = Vector Double
+
+type MatParam = Isect -> Color
 
 data Material = Material {ka :: MatParam,
                           ks :: MatParam,
                           kd :: MatParam,
                           ke :: MatParam,
                           kr :: MatParam,
-                          kt :: MatParam}
+                          kt :: MatParam,
+                          shininess :: Isect -> Double}
 
-data Light =    PointLight       {lColor :: Vector Double,
+data Light =    PointLight       {lColor :: Color,
                                   lPosition :: Vector Double,
                                   constantTerm :: Double,
                                   linearTerm :: Double,
                                   quadraticTerm :: Double}
-            |   DirectionalLight {lColor :: Vector Double,
+            |   DirectionalLight {lColor :: Color,
                                   lOrientation :: Vector Double}
     deriving (Show, Eq)
 
 type Lights = [Light]
 
-lightColor :: Light -> Vector Double
+lightColor :: Light -> Color
 lightColor (PointLight c _ _ _ _) = c
 lightColor (DirectionalLight c _) = c
 
@@ -57,6 +63,10 @@ distanceAttenuation (PointLight pos _ c b a) p = min 1.0 atten
           dist = norm_2 (p - pos)
 distanceAttenuation (DirectionalLight _ _) p = 1.0
 
+{- TODO: this requires scene implementation
+shadowAttenuation :: Light -> Ray -> Vector Double -> Vector Double
+-}
+
 data Geometry = Sphere {sphereCenter :: Vector Double,
                         sphereRadius :: Double,
                         sphereMaterial :: Material}
@@ -67,9 +77,9 @@ instance Show Geometry where
 
 type Objects = [Geometry]
 
-data Scene = Scene {lights :: Lights,
-                    objects :: Objects,
-                    ambient :: Vector Double}
+data Scene = Scene {scLights :: Lights,
+                    scObjects :: Objects,
+                    scAmbient :: Color}
 
 intersect :: Geometry -> Ray -> Maybe Isect
 intersect s@(Sphere center radius material) r@(Ray rayPos rayDir) =
@@ -84,11 +94,42 @@ intersect s@(Sphere center radius material) r@(Ray rayPos rayDir) =
         if t2 < 0.0 then
             Nothing
         else if t1 > 0.0 then
-            pure (Isect t1 (at t1 r) s material)
+            pure (Isect t1 (normalize (at t1 r)) s material)
         else
-            pure (Isect t2 (at t2 r) s material)
+            pure (Isect t2 (normalize (at t2 r)) s material)
 
+perLightDiffuse :: Light -> Vector Double -> Vector Double -> Vector Double -> Double
+perLightDiffuse light isectPoint rayDir normal = 
+    let lightDir = getDirection light isectPoint in 
+    let ddot = lightDir <.> (if (rayDir <.> normal > 0) then -normal else normal) in
+    let dmax = max ddot 0.0 in
+    dmax
 
-{- TODO: this requires scene implementation
-shadowAttenuation :: Ray -> Vector Double -> Vector Double
--}
+perLightSpecular :: Light -> Vector Double -> Vector Double -> Vector Double -> Double
+perLightSpecular light isectPoint rayDir normal =
+    let lightDir = getDirection light isectPoint in 
+    let reflect = lightDir - (fromList [2 * (lightDir <.> normal)]) * normal in
+    if norm_2 reflect > 0 then
+        0
+    else
+        let reflectNorm = normalize reflect in
+        let sdot = rayDir <.> reflect in
+        let smax = max sdot 0.0 in
+        smax
+
+combineLightTerms :: Vector Double -> Vector Double -> Vector Double -> Double -> Light -> Color
+combineLightTerms isectPoint rayDir normal alpha light =
+    let diffuse = perLightDiffuse light isectPoint rayDir normal in
+    let specular = perLightSpecular light isectPoint rayDir normal in
+    let color = lightColor light in
+    let atten = distanceAttenuation light isectPoint in
+    color * (fromList [(diffuse + specular) * atten])
+
+shade :: Scene -> Ray -> Isect -> Color
+shade (Scene lights objects ambient) r@(Ray p d) i@(Isect t normal _ material) = 
+    let (Material ka ks kd ke kr kt shiny) = material in
+    let emissive = ke i in
+    let amb = (ka i) * ambient in
+    let isectPoint = at t r in
+    let alpha = shiny i in
+    emissive + amb + (sum . map (combineLightTerms isectPoint d normal alpha) $ lights)
